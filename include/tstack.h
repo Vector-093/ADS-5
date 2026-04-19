8fe87bfedd3cc02a302facb4b8ee5c39e47afd8a
Vector-093 <motleo2007@gmail.com>
2026-04-19 19:19:24 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,40 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T m[size];
+    int top_i;
+
+public:
+    TStack() : top_i(-1) {}
+
+    void push(T value) {
+        if (top_i < size - 1) {
+            m[++top_i] = value;
+        }
+    }
+
+    T pop() {
+        if (top_i >= 0) {
+            return m[top_i--];
+        }
+        return T();
+    }
+
+    T top() const {
+        if (top_i >= 0) {
+            return m[top_i];
+        }
+        return T();
+    }
+
+    bool isEmpty() const {
+        return top_i == -1;
+    }
+
+    bool isFull() const {
+        return top_i == size - 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
